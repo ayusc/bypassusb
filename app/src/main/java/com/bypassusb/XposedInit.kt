6f92694a52cabac76102ba367b63fa41bf58c885
Ayus Chatterjee <144832717+ayusc@users.noreply.github.com>
2026-06-07 19:45:45 +0530

    Update XposedInit.kt

--- app/src/main/java/com/bypassusb/XposedInit.kt
+++ app/src/main/java/com/bypassusb/XposedInit.kt
@@ -8,78 +8,86 @@ import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam
 
 class UsbStealthModule : IXposedHookLoadPackage {
     override fun handleLoadPackage(lpparam: LoadPackageParam) {
-    
-        if (lpparam.packageName != "com.sbi.upi") return
+      
+        val targetApp = "com.sbi.upi" 
+        
+        if (lpparam.packageName != targetApp) return
 
-        XposedBridge.log("Stealth USB module active for com.sbi.upi")
+        XposedBridge.log("[UsbStealth] Loaded into $targetApp")
 
-        XposedHelpers.findAndHookMethod(
-            "android.content.Intent",
-            lpparam.classLoader,
-            "getIntExtra",
-            String::class.java,
-            Int::class.javaPrimitiveType,
-            object : XC_MethodHook() {
-                override fun afterHookedMethod(param: MethodHookParam) {
-                    val key = param.args[0] as String
-                    if (key == "plugged") {
-                        val originalValue = param.result as Int
-                        if (originalValue == 2) { // 2 = BATTERY_PLUGGED_USB
-                            param.result = 1     // 1 = BATTERY_PLUGGED_AC
+        try {
+            XposedHelpers.findAndHookMethod(
+                "android.content.Intent",
+                lpparam.classLoader,
+                "getIntExtra",
+                String::class.java,
+                Int::class.javaPrimitiveType,
+                object : XC_MethodHook() {
+                    override fun afterHookedMethod(param: MethodHookParam) {
+                        val key = param.args[0] as String
+                        if (key == "plugged") {
+                            val originalValue = param.result as Int
+                            if (originalValue == 2) { // 2 = BATTERY_PLUGGED_USB
+                                param.result = 1     // 1 = BATTERY_PLUGGED_AC
+                                // Optional: Log when the spoof actually happens
+                                // XposedBridge.log("[UsbStealth] Spoofed plugged state from USB to AC")
+                            }
                         }
                     }
                 }
-            }
-        )
+            )
 
-        XposedHelpers.findAndHookMethod(
-            "android.content.Intent",
-            lpparam.classLoader,
-            "getBooleanExtra",
-            String::class.java,
-            Boolean::class.javaPrimitiveType,
-            object : XC_MethodHook() {
-                override fun afterHookedMethod(param: MethodHookParam) {
-                    val key = param.args[0] as String
-                    if (key == "connected" || key == "host_connected") {
-                        param.result = false // Deny that a host PC is connected
+            XposedHelpers.findAndHookMethod(
+                "android.content.Intent",
+                lpparam.classLoader,
+                "getBooleanExtra",
+                String::class.java,
+                Boolean::class.javaPrimitiveType,
+                object : XC_MethodHook() {
+                    override fun afterHookedMethod(param: MethodHookParam) {
+                        val key = param.args[0] as String
+                        if (key == "connected" || key == "host_connected") {
+                            param.result = false 
+                        }
                     }
                 }
-            }
-        )
+            )
 
-        XposedHelpers.findAndHookMethod(
-            "android.os.SystemProperties",
-            lpparam.classLoader,
-            "get",
-            String::class.java,
-            String::class.java,
-            object : XC_MethodHook() {
-                override fun beforeHookedMethod(param: MethodHookParam) {
-                    val key = param.args[0] as String
-                    if (key == "sys.usb.state" || key == "sys.usb.config") {
-                        param.result = "none"
+            XposedHelpers.findAndHookMethod(
+                "android.os.SystemProperties",
+                lpparam.classLoader,
+                "get",
+                String::class.java,
+                String::class.java,
+                object : XC_MethodHook() {
+                    override fun beforeHookedMethod(param: MethodHookParam) {
+                        val key = param.args[0] as String
+                        if (key == "sys.usb.state" || key == "sys.usb.config") {
+                            param.result = "none"
+                        }
                     }
                 }
-            }
-        )
+            )
 
-        val targetSettingsKeys = listOf("usb_mass_storage_enabled", "adb_enabled")
-        XposedHelpers.findAndHookMethod(
-            "android.provider.Settings\$Global",
-            lpparam.classLoader,
-            "getInt",
-            android.content.ContentResolver::class.java,
-            String::class.java,
-            Int::class.javaPrimitiveType,
-            object : XC_MethodHook() {
-                override fun beforeHookedMethod(param: MethodHookParam) {
-                    val key = param.args[1] as String
-                    if (targetSettingsKeys.contains(key)) {
-                        param.result = 0 
+            val targetSettingsKeys = listOf("usb_mass_storage_enabled", "adb_enabled")
+            XposedHelpers.findAndHookMethod(
+                "android.provider.Settings\$Global",
+                lpparam.classLoader,
+                "getInt",
+                android.content.ContentResolver::class.java,
+                String::class.java,
+                Int::class.javaPrimitiveType,
+                object : XC_MethodHook() {
+                    override fun beforeHookedMethod(param: MethodHookParam) {
+                        val key = param.args[1] as String
+                        if (targetSettingsKeys.contains(key)) {
+                            param.result = 0 
+                        }
                     }
                 }
-            }
-        )
+            )
+        } catch (t: Throwable) {
+            XposedBridge.log("[UsbStealth] Error hooking $targetApp: ${t.message}")
+        }
     }
 }
